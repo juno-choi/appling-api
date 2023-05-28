6ac129e72efc067528110547793b8b916d8fa6d0
juno-choi <ililil9482@naver.com>
2023-05-28 14:32:22 +0900

    [설정] 시큐리티 설정 3.0 방식으로 변경

--- src/main/java/com/juno/appling/common/security/SecurityConfig.java
+++ src/main/java/com/juno/appling/common/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.juno.appling.common.security;
 
+import com.juno.appling.domain.enums.member.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,9 +20,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf().disable()
-                .cors().disable()
-                .headers().frameOptions().disable().disable()
+        http.csrf(c -> c.disable())
+                .cors(c -> c.disable())
+                .headers(c -> c.frameOptions(f -> f.disable()).disable())
                 .authorizeHttpRequests(auth -> {
                     try{
                         auth.requestMatchers(WHITE_LIST).permitAll()
