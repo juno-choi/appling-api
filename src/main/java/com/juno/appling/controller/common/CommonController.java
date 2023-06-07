2b7d22b6c179140da56cc93d818e506333c7d676
juno <juno.c@kakaovx.com>
2023-06-07 19:06:54 +0900

    [수정] ${api-prefix} 값이 빠져있어서 수정

--- src/main/java/com/juno/appling/controller/common/CommonController.java
+++ src/main/java/com/juno/appling/controller/common/CommonController.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/common")
+@RequestMapping("${api-prefix}/common")
 @RequiredArgsConstructor
 public class CommonController {
     private final CommonS3Service commonS3Service;
