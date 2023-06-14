55ea9b1a5f3d216c5fb3797934da247f6e656825
juno <juno.c@kakaovx.com>
2023-06-14 15:21:53 +0900

    [문서] 카카오 로그인 문서 작성

--- src/test/java/com/juno/appling/controller/member/OAuthControllerDocs.java
+++ src/test/java/com/juno/appling/controller/member/OAuthControllerDocs.java
@@ -35,6 +35,9 @@ class OAuthControllerDocs extends BaseTest {
     @MockBean(name = "kakaoClient")
     private WebClient kakaoClient;
 
+    @MockBean(name = "kakaoApiClient")
+    private WebClient kakaoApiClient;
+
     private static WebClient webClient;
 
     @BeforeAll
@@ -85,6 +88,72 @@ class OAuthControllerDocs extends BaseTest {
                         fieldWithPath("data.refresh_token").type(JsonFieldType.STRING).description("카카오에서 발급된 refresh token"),
                         fieldWithPath("data.access_token_expired").type(JsonFieldType.NUMBER).description("access token expired"),
                         fieldWithPath("data.refresh_token_expired").type(JsonFieldType.NUMBER).description("refresh token expired")
+                )
+        ));
+    }
+
+    @Test
+    @DisplayName(PREFIX+"/kakao/login")
+    void oauthKakaoLogin() throws Exception {
+        given(kakaoApiClient.post()).willReturn(webClient.post());
+
+        //given
+        String kakaoReturnString = "{\n" +
+                "    \"id\": 2727704352,\n" +
+                "    \"connected_at\": \"2023-03-29T23:31:57Z\",\n" +
+                "    \"properties\": {\n" +
+                "        \"nickname\": \"최준호\",\n" +
+                "        \"profile_image\": \"http://k.kakaocdn.net/dn/cpEwh1/btsfeggLuQz/AxpdbDgopjdSz6a36cjksK/img_640x640.jpg\",\n" +
+                "        \"thumbnail_image\": \"http://k.kakaocdn.net/dn/cpEwh1/btsfeggLuQz/AxpdbDgopjdSz6a36cjksK/img_110x110.jpg\"\n" +
+                "    },\n" +
+                "    \"kakao_account\": {\n" +
+                "        \"profile_nickname_needs_agreement\": false,\n" +
+                "        \"profile_image_needs_agreement\": false,\n" +
+                "        \"profile\": {\n" +
+                "            \"nickname\": \"최준호\",\n" +
+                "            \"thumbnail_image_url\": \"http://k.kakaocdn.net/dn/cpEwh1/btsfeggLuQz/AxpdbDgopjdSz6a36cjksK/img_110x110.jpg\",\n" +
+                "            \"profile_image_url\": \"http://k.kakaocdn.net/dn/cpEwh1/btsfeggLuQz/AxpdbDgopjdSz6a36cjksK/img_640x640.jpg\",\n" +
+                "            \"is_default_image\": false\n" +
+                "        },\n" +
+                "        \"has_email\": true,\n" +
+                "        \"email_needs_agreement\": false,\n" +
+                "        \"is_email_valid\": true,\n" +
+                "        \"is_email_verified\": true,\n" +
+                "        \"email\": \"ililil9482@naver.com\",\n" +
+                "        \"has_age_range\": true,\n" +
+                "        \"age_range_needs_agreement\": false,\n" +
+                "        \"age_range\": \"30~39\",\n" +
+                "        \"has_birthday\": true,\n" +
+                "        \"birthday_needs_agreement\": false,\n" +
+                "        \"birthday\": \"1030\",\n" +
+                "        \"birthday_type\": \"SOLAR\",\n" +
+                "        \"has_gender\": true,\n" +
+                "        \"gender_needs_agreement\": false,\n" +
+                "        \"gender\": \"male\"\n" +
+                "    }\n" +
+                "}";
+        mockWebServer.enqueue(new MockResponse().setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON).setBody(kakaoReturnString));
+
+        //when
+        ResultActions resultActions = mock.perform(
+                get(PREFIX + "/kakao/login").param("access_token", "kakao_access_token")
+        ).andDo(print());
+
+        //then
+        resultActions.andExpect(status().is2xxSuccessful());
+
+        //docs
+        resultActions.andDo(docs.document(
+                queryParameters(
+                        parameterWithName("access_token").description("kakao login access token")
+                ),
+                responseFields(
+                        fieldWithPath("code").type(JsonFieldType.STRING).description("결과 코드"),
+                        fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메세지"),
+                        fieldWithPath("data.type").type(JsonFieldType.STRING).description("token type"),
+                        fieldWithPath("data.access_token").type(JsonFieldType.STRING).description("Appling에서 발급된 access token"),
+                        fieldWithPath("data.refresh_token").type(JsonFieldType.STRING).description("Appling에서 발급된 refresh token"),
+                        fieldWithPath("data.access_token_expired").type(JsonFieldType.NUMBER).description("access token expired")
                 )
         ));
     }
