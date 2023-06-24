c5ca76e16b052f6e9776c7f6552486d1b760220d
juno-choi <ililil9482@naver.com>
2023-06-24 19:02:35 +0900

    [설정] querydsl bean 추가

---
+++ src/main/java/com/juno/appling/common/querydsl/QuerydslConfig.java
@@ -0,0 +1,18 @@
+package com.juno.appling.common.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuerydslConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory query(){
+        return new JPAQueryFactory(entityManager);
+    }
+}
