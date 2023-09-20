7dc0e0b32003b27f7b023c05169cf0bf5133d843
juno <juno.c@kakaovx.com>
2023-09-21 08:32:37 +0900

    [docs] docs 주소 변경

--- src/main/java/com/juno/appling/global/webconfig/WebConfig.java
+++ src/main/java/com/juno/appling/global/webconfig/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/docs/**")
+        registry.addResourceHandler("/api/docs/**")
             .addResourceLocations("classpath:/static/docs/");
     }
 }
