94a64d5e55adeceb39ea03532fc57bc278dcb505
juno <juno.c@kakaovx.com>
2023-09-21 12:25:26 +0900

    [fix] security config allowed method 수정2

--- src/main/java/com/juno/appling/global/security/SecurityConfig.java
+++ src/main/java/com/juno/appling/global/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
                 @Override
                 public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Collections.singletonList("*"));
+                    config.setAllowedOriginPatterns(Collections.singletonList("*"));
                     config.setAllowedMethods(Collections.singletonList("*"));
                     config.setAllowCredentials(true);
                     config.setAllowedHeaders(Collections.singletonList("*"));
