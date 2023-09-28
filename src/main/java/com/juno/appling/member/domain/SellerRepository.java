66d5c56e7fca419823e8a8a429c1154a82d0acab
juno-choi <ililil9482@naver.com>
2023-09-28 21:15:54 +0900

    [fix] seller 신청시 회사명 중복검사 추가

--- src/main/java/com/juno/appling/member/domain/SellerRepository.java
+++ src/main/java/com/juno/appling/member/domain/SellerRepository.java
@@ -1,7 +1,5 @@
 package com.juno.appling.member.domain;
 
-import com.juno.appling.member.domain.Member;
-import com.juno.appling.member.domain.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +7,6 @@ import java.util.Optional;
 public interface SellerRepository extends JpaRepository<Seller, Long> {
 
     Optional<Seller> findByMember(Member member);
+
+    Optional<Seller> findByCompany(String company);
 }
