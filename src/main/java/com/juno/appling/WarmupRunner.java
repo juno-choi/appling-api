496d263962a04646347b0c0d8bfe9c6cd9efc934
juno-choi <ililil9482@naver.com>
2023-10-29 10:56:14 +0900

    [fix] warm up 로그만 남겨두고 제거

--- src/main/java/com/juno/appling/WarmupRunner.java
+++ src/main/java/com/juno/appling/WarmupRunner.java
@@ -1,25 +1,19 @@
 package com.juno.appling;
 
-import com.juno.appling.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class WarmupRunner implements ApplicationListener<ApplicationReadyEvent> {
-
-    private final ProductService productService;
-
-    public static boolean isWarmup = false;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         // warmup
-        Pageable pageable = Pageable.ofSize(10);
-        productService.getProductList(pageable, "", "normal", 0L, 0L);
-        isWarmup = true;
+        log.info("warmup");
     }
 }
